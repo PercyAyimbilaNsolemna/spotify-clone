f589f6baf1de110e15030c599f80d73d1d9c0276
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-04-03 08:40:46 +0000

    Fix prettier errors.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    //Applies the middleware to the entire route
+    consumer.apply(LoggerMiddleware).forRoutes('songs');
+    //Applies the middleware to only the POST request in the songs route
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes({ path: 'songs', method: RequestMethod.POST });
+    //Applies the middleware to the entire route or controller
+    //consumer.apply(LoggerMiddleware).forRoutes(SongsController);
+  }
+}
