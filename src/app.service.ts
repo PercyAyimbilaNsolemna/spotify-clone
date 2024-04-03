51b8237ed393cc062346583c57908374efe3e75a
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-04-03 15:50:37 +0000

    Add Dev Config Service.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { DevConfigService } from './common/providers/DevConfigService';
 
 @Injectable()
 export class AppService {
+  constructor(private devConfigService: DevConfigService) {}
   getHello(): string {
+    console.log(this.devConfigService.getBDHOST());
     return 'Hello World!';
   }
 }
